7161a3e51f07856ab73bd4fe62e326c56084ac71
Virakti Jain <viraktijain@commvault.com>
2022-09-13 21:22:25 +0530

    Updating the files

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let keepElem = document.getElementById("keep")
 let bookmark;
 let bookmarkIndex = 0;
 let bookmarkArr;
+let folderIndex;
 let url;
 chrome.bookmarks.getTree((node) => {
     chrome.storage.sync.get("folder", (data) => {
@@ -17,7 +18,8 @@ chrome.bookmarks.getTree((node) => {
         bookmarkIndex = data.bookmarkIndex;
 
         chrome.storage.sync.get("folderIndex", (data) => {
-            bookmarkArr = node[0].children[data.folderIndex];
+            folderIndex = data.folderIndex;
+            bookmarkArr = node[0].children[folderIndex];
             bookmark = bookmarkArr.children[bookmarkIndex];
             assignBookmark(bookmark);
 
@@ -31,42 +33,74 @@ chrome.bookmarks.getTree((node) => {
 })
 function setNextBookmark(index) {
     let nextBookmark;
+    console.log(bookmarkArr.children)
+    //debugger;
     if (index < bookmarkArr.children.length) {
         bookmarkIndex = index;
         nextBookmark = bookmarkArr.children[bookmarkIndex];
         chrome.storage.sync.set({ bookmarkIndex: bookmarkIndex });
     }
+
+
     return nextBookmark;
 
 }
 function assignBookmark(bookmarkObj) {
+    if (bookmarkObj) {
+        console.log('Bookmark object is', bookmarkObj)
+        bookmarkElem.innerText = bookmarkObj.title;
+        bookmarkElem.href = bookmarkObj.url;
 
-    console.log('Bookmark object is', bookmarkObj)
-    bookmarkElem.innerText = bookmarkObj.title;
-    bookmarkElem.href = bookmarkObj.url;
+        url = bookmarkObj.url;
+        console.log(bookmarkObj);
+    }
+    else {
 
-    url = bookmarkObj.url;
-    bookmarkElem.addEventListener("click", (event) => {
-        console.log(`Redirecting to ${url}`);
-        chrome.tabs.create({ url: url });
+        document.getElementById("form").style.display = "none";
+        document.getElementById("content").innerText = "Reached the end!"
+    }
 
-    });
+
 }
+bookmarkElem.addEventListener("click", (event) => {
+    console.log(`Redirecting to ${url}`);
+    chrome.tabs.create({ url: url });
+
+
+});
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    console.log(changes)
+    console.log(namespace)
+})
 
 removeElem.addEventListener("click", (event) => {
     event.preventDefault();
     console.log(bookmark)
     chrome.bookmarks.remove((bookmark.id), () => {
         console.log("Removed the bookmark.");
+        chrome.bookmarks.getTree((node) => {
 
-        setNewBookmark(bookmarkIndex);
+
+            bookmarkArr = node[0].children[folderIndex];
+            console.log(bookmarkArr);
+            //debugger;
+            setNewBookmark(bookmarkIndex);
+        });
+
+
+
 
     })
+
+
 })
+
 function setNewBookmark(index) {
 
     let nextBookmark = setNextBookmark(index);
+
     assignBookmark(nextBookmark);
+
 }
 
 
